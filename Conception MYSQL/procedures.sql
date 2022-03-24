935c1c87170bdca029f3a74b7b93383ea86a2288
AntonyCollin <72162477+AntonyCollin@users.noreply.github.com>
2022-03-24 01:25:40 -0400

    Add files via upload

--- Conception MYSQL/procedures.sql
+++ Conception MYSQL/procedures.sql
@@ -12,7 +12,6 @@ DELIMITER $$
 CREATE PROCEDURE AjouterArmure
 (nom VARCHAR(45),quantite int(11), type char(1), prixUnitaire decimal(10,0), poids float, description varchar(120), estEnVente bit(1), matiere varchar(30), taille int(11))
 BEGIN
-DECLARE IdItems int;
 	IF TRIM(nom) != '' AND quantite >= 0 AND TRIM(type) != '' AND prixUnitaire > 0 AND poids > 0 AND estEnVente IS NOT NULL AND TRIM(matiere) != '' AND taille > 0
     THEN
 			INSERT INTO Items(nom, quantite, type, prixUnitaire, poids, description, estEnVente)
@@ -26,8 +25,8 @@ DECLARE IdItems int;
 	END IF;    
 END $$
 
-call AjouterArmure('antozItem', 69, 'A', 69, 69, '', 0, 'antozMatiere', 69);
+#call AjouterArmure('antozItem', 69, 'A', 69, 71, '', 0, 'antozMatiere', 69);
 
-select * from Items inner join Armures on Items.IdItems = Armures.Items_IdItems;
-select * from Armures;
-select * from Items;
+#select * from Items inner join Armures on Items.IdItems = Armures.Items_IdItems;
+#select * from Armures;
+#select * from Items;
