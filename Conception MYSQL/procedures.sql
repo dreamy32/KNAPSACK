560b0a484cf5b7d6360081a2702a2cf3ad4e712c
antony <antony.collin.d@gmail.com>
2022-03-23 22:19:07 -0400

    Procédures de Antoz
    Ajouter Armure

---
+++ Conception MYSQL/procedures.sql
@@ -0,0 +1,33 @@
+/*
++--------------------------------------------+----------+
+|            Membres de l'Équipe             |  Projet  |
++--------------------------------------------+----------+
+| Amélie Bouchard | Antony Collin-Desrochers | KNAPSACK |
+| David Bérubé    | Samy Tétrault            |          |
++--------------------------------------------+----------+
+*/ 
+
+#Ajouter Armure
+DELIMITER $$
+CREATE PROCEDURE AjouterArmure
+(nom VARCHAR(45),quantite int(11), type char(1), prixUnitaire decimal(10,0), poids float, description varchar(120), estEnVente bit(1), matiere varchar(30), taille int(11))
+BEGIN
+DECLARE IdItems int;
+	IF TRIM(nom) != '' AND quantite >= 0 AND TRIM(type) != '' AND prixUnitaire > 0 AND poids > 0 AND estEnVente IS NOT NULL AND TRIM(matiere) != '' AND taille > 0
+    THEN
+			INSERT INTO Items(nom, quantite, type, prixUnitaire, poids, description, estEnVente)
+			VALUES(nom, quantite, type, prixUnitaire,poids, description, estEnVente);
+			SET @IdItems = LAST_INSERT_ID();
+			INSERT INTO Armures(Items_IdItems, taille, matiere)
+			VALUES(@IdItems, taille, matiere);
+	COMMIT;
+        ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Les valeurs sont mauvaises';
+	END IF;    
+END $$
+
+call AjouterArmure('antozItem', 69, 'A', 69, 69, '', 0, 'antozMatiere', 69);
+
+select * from Items inner join Armures on Items.IdItems = Armures.Items_IdItems;
+select * from Armures;
+select * from Items;
