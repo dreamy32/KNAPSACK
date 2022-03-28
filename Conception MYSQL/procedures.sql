815b81f39b121aa851aa5a55f4fa53003626097c
AntonyCollin <72162477+AntonyCollin@users.noreply.github.com>
2022-03-28 17:55:27 -0400

    Add files via upload
    Procédures, fonctions, triggers de Antony -- 28 Mars 2022

--- Conception MYSQL/procedures.sql
+++ Conception MYSQL/procedures.sql
@@ -4,7 +4,7 @@
 +--------------------------------------------+----------+
 | Amélie Bouchard | Antony Collin-Desrochers | KNAPSACK |
 | David Bérubé    | Samy Tétrault            |          |
-+--------------------------------------------+----------+
++--------------------------------------------+----s------+
 */ 
 
 #Ajouter Armure
@@ -25,16 +25,16 @@ BEGIN
 	END IF;    
 END $$
 
-#Afficher objets en vente sans les détails de son type [Mettre % entre paramètre pour afficher tout]
+#Afficher objets en vente sans les détails de son type
 DELIMITER $$
-CREATE PROCEDURE AfficherItemsVente(tType char(1))
+CREATE PROCEDURE AfficherItemsVente()
 BEGIN
-	SELECT nom, quantite, type, prixUnitaire, poids, description from Items where estEnVente = 1 and type LIKE tType;
+	SELECT nom, quantite, type, prixUnitaire, poids, description from Items where estEnVente = 1;
 END $$
 
 #Afficher les details selon le type d'un item
 DELIMITER $$
-CREATE PROCEDURE AfficherItemDetails(IdItemsP int)
+CREATE PROCEDURE AfficherItemDetails(IdItemsP int(11))
 BEGIN
 DECLARE typeItem char(1);
 	IF IdItemsP > 0 THEN
@@ -65,44 +65,71 @@ DECLARE typeItem char(1);
 	END IF;    
 END $$
 
-/*
-#Supprimer Item    FAIRE LE TRIGGER AVANT
+#Supprimer Item
 DELIMITER $$
 CREATE PROCEDURE SupprimerItem(IdItemsP int(11))
 BEGIN
 DECLARE typeItem char(1);
 	IF IdItemsP > 0 THEN
-			SET typeItem = (SELECT type FROM Items WHERE IdItems = IdItemsP);
-
-            IF typeItem = 'W' THEN
-            DELETE FROM Armes WHERE Items_IdItems = IdItemsP;
-            DELETE FROM Items WHERE IdItems = IdItemsP;
-            END IF;
-            
-            IF typeItem = 'D' THEN
-            DELETE FROM Armures WHERE Items_IdItems = IdItemsP;
-            DELETE FROM Items WHERE IdItems = IdItemsP;
-            END IF;
-            
-            IF typeItem = 'A' THEN
-            DELETE FROM Nourriture WHERE Items_IdItems = IdItemsP;
-            DELETE FROM Items WHERE IdItems = IdItemsP;
-            END IF;
-            
-            IF typeItem = 'N' THEN
-            DELETE FROM Munitions WHERE Items_IdItems = IdItemsP;
-            DELETE FROM Items WHERE IdItems = IdItemsP;
-            END IF;
-
-            IF typeItem = 'M' THEN
-            DELETE FROM Medicament WHERE Items_IdItems = IdItemsP;
-            DELETE FROM Items WHERE IdItems = IdItemsP;
-            END IF;
-            COMMIT;
+			DELETE FROM Items WHERE IdItems = IdItemsP;
     ELSE
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Le id item est introuvable';
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Le id item est invalide';
 	END IF;    
-END $$*/
+END $$
+
+#Trigger Supprimer
+delimiter |
+create trigger SupprimerItemTrigger
+before delete on Items
+for each row
+begin
+            IF old.type = 'W' THEN
+            DELETE FROM Armes WHERE Items_IdItems = old.IdItems;
+            END IF;
+            
+            IF old.type = 'A' THEN
+            DELETE FROM Armures WHERE Items_IdItems = old.IdItems;
+            END IF;
+            
+            IF old.type = 'N' THEN
+            DELETE FROM Nourriture WHERE Items_IdItems = old.IdItems;
+            END IF;
+            
+            IF old.type = 'M' THEN
+            DELETE FROM Munitions WHERE Items_IdItems = old.IdItems;
+            END IF;
+
+            IF old.type = 'D' THEN
+            DELETE FROM Medicament WHERE Items_IdItems = old.IdItems;
+            END IF;
+            
+            IF EXISTS (SELECT Items_IdItems FROM Evaluations WHERE Items_IdItems = old.IdItems) THEN
+            DELETE FROM Evaluations WHERE Items_IdItems = old.IdItems;
+            END IF;
+            
+            IF EXISTS (SELECT Items_IdItems FROM Inventaire WHERE Items_IdItems = old.IdItems) THEN
+            DELETE FROM Inventaire WHERE Items_IdItems = old.IdItems;
+            END IF;
+            
+            IF EXISTS (SELECT Items_IdItems FROM Panier WHERE Items_IdItems = old.IdItems) THEN
+            DELETE FROM Panier WHERE Items_IdItems = old.IdItems;
+            END IF;
+end |
+
+#Ajouter Évaluation
+DELIMITER $$
+CREATE PROCEDURE AjouterÉvaluation
+(Items_IdItemsP int(11),Joueurs_IdJoueurP int(11), commentaire varchar(300), nbEtoiles int(5))
+BEGIN
+	IF EXISTS (SELECT IdItems FROM Items WHERE IdItems = Items_IdItemsP) AND EXISTS (SELECT IdJoueur FROM Joueurs WHERE IdJoueur = Joueurs_IdJoueurP) AND TRIM(commentaire) != '' AND nbEtoiles >= 0 AND nbEtoiles <= 5 
+    THEN
+			INSERT INTO Evaluations(Items_IdItems,Joueurs_IdJoueur, commentaire, nbEtoiles)
+			VALUES(Items_IdItemsP,Joueurs_IdJoueurP, commentaire, nbEtoiles);
+	COMMIT;
+        ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Les valeurs sont mauvaises';
+	END IF;    
+END $$
 
 #Ajouter Medicament
 DELIMITER $$
@@ -158,24 +185,6 @@ BEGIN
 	END IF;    
 END $$
 
-#Ajouter Munitions
-DELIMITER $$
-CREATE PROCEDURE AjouterMunitions
-(nom VARCHAR(45),quantite int(11), type char(1), prixUnitaire decimal(10,0), poids float, description varchar(120), estEnVente bit(1), calibre float)
-BEGIN
-	IF TRIM(nom) != '' AND quantite >= 0 AND TRIM(type) != '' AND prixUnitaire > 0 AND poids > 0 AND (estEnVente = 0 OR estEnVente = 1) AND calibre > 0
-    THEN
-			INSERT INTO Items(nom, quantite, type, prixUnitaire, poids, description, estEnVente)
-			VALUES(nom, quantite, type, prixUnitaire,poids, description, estEnVente);
-			SET @IdItems = LAST_INSERT_ID();
-			INSERT INTO Munitions(Items_IdItems, calibre)
-			VALUES(@IdItems, calibre);
-	COMMIT;
-        ELSE
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Les valeurs sont mauvaises';
-	END IF;    
-END $$
-
 #Ajouter Armes
 DELIMITER $$
 CREATE PROCEDURE AjouterArmes
@@ -193,22 +202,3 @@ BEGIN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Les valeurs sont mauvaises';
 	END IF;    
 END $$
-/*
-call AfficherItemsVente();
-call AjouterArmure('Item6', 69, 'A', 69, 71, 'description basique', 1, 'antozMatiere', 69);
-call AjouterMedicament('med', 10, 'D', 10, 71, 'description basique', 1, 'boost de vie', 10);
-call AjouterNourriture('bouffe', 10, 'N', 10, 71, 'description basique', 1, 2);
-call AjouterMunitions('munition', 10, 'N', 10, 71, 'description basique', 1, 5.56);
-call AjouterArmes('arme', 10, 'W', 10, 71, 'description basique', 1, 5, 'lourd', 30);
-call SupprimerItem(28);
-        
-select * from Items inner join Armes on Items.IdItems = Armes.Items_IdItems;
-
-#select * from Items inner join Armures on Items.IdItems = Armures.Items_IdItems;
-select * from Items inner join Medicament on Items.IdItems = Medicament.Items_IdItems;
-select * from Items inner join Nourriture on Items.IdItems = Nourriture.Items_IdItems;
-select * from Items inner join Munitions on Items.IdItems = Munitions.Items_IdItems;
-select * from Items inner join Armes on Items.IdItems = Armes.Items_IdItems;
-SELECT * FROM Medicament;
-select * from Items;*/
-call AfficherItemsVente('%');
