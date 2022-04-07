9721a2a1777175b3eb5668331a202ee4abe8728e
SamyTetrault <101147474+SamyTetrault@users.noreply.github.com>
2022-04-06 20:31:00 -0400

    Update menuPopUp.js

--- Pd-KnapSack/js/menuPopUp.js
+++ Pd-KnapSack/js/menuPopUp.js
@@ -2,11 +2,10 @@ function afficherMenu() {
     var menu = document.getElementById("MenuPopUp");
     menu.classList.toggle("show");
   }
-  function afficherRecherche() {
-    var recherche = document.getElementById("RecherchePopUp");
-    recherche.classList.toggle("show");
-  }
-
+function afficherRecherche() {
+  var recherche = document.getElementById("RecherchePopUp");
+  recherche.classList.toggle("show");
+}
 var estOuvert = false 
 var dernierIdDiv = 0;
 function afficherMenuItem(idItem) {
