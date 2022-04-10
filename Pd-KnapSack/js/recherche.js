a0709ba38b04a2fd6373dfc8701ec5a7feb82c15
antony <antony.collin.d@gmail.com>
2022-04-10 17:16:01 -0400

    transfer js trier function to reccherche.js
    yooo

--- Pd-KnapSack/js/recherche.js
+++ Pd-KnapSack/js/recherche.js
@@ -32,3 +32,97 @@ const ResetCheckboxes = (checkboxesArr) => {
     
   }
 };
+
+function trier()
+{
+    var url = "";
+    var count = 0;
+
+    document.getElementsByName("tri").forEach(checkbox => {
+        if (checkbox.checked)
+        {
+          if (count != 0)
+          {
+            url += "," + checkbox.value;
+            
+          }
+          else
+          {
+            url = "?tri=";
+            url += checkbox.value;
+          }
+          count++;
+        }
+    });  
+  
+    if (document.getElementById("nbEtoiles").value != 0)
+    {
+    if (count == "")
+    {
+      url = "?nbEtoiles=" + document.getElementById("nbEtoiles").value;
+    }
+    else
+    {
+      url += "&" + "nbEtoiles=" + document.getElementById("nbEtoiles").value;
+    }
+  }
+
+  $countType = 0;
+
+document.getElementsByName("type").forEach(checkbox => {
+  if (checkbox.checked)
+  {
+    $countType++;
+
+    if ($countType == 1)
+    {
+  if (url == "")
+  {
+    url = "?type=" + checkbox.value;
+  }
+  else
+  {
+    url += "&" + "type=" + checkbox.value;
+  }
+}
+else
+{
+  url += "-" + checkbox.value;
+}
+}});
+
+
+  if (url != "")
+  {
+    if (document.getElementById("order-button").value === "false")
+    {
+      url += "&ordre=" + "ASC";
+    }
+    else
+    {
+      url += "&ordre=" + "DESC";
+    }
+  }
+  else
+  {
+    if (document.getElementById("order-button").value === "false")
+    {
+      url += "?ordre=" + "ASC";
+    }
+    else
+    {
+      url += "?ordre=" + "DESC";
+    }
+  }
+
+    
+  if (url == "")
+  {
+    window.history.pushState({}, document.title, "/" + "PhpBuilding");
+    location.reload();
+  }
+  else
+  {
+    window.location.href = url;
+  }
+}
